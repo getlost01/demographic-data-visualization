f8f3aa945b3af063c2d649d1e8c4537f02f8d16a
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-12-01 12:00:16 +0530

    create the insta campus minor web page

--- app.js
+++ app.js
@@ -1,46 +1,97 @@
-$(document).ready(function () {
-    // Your routing logic can go here if needed
+const searchInput = document.querySelector("#search");
+const searchButton = document.querySelector("#search-button");
 
-    // Event listener for file input change
-    $('#fileInput').on('change', function (e) {
-        var file = e.target.files[0];
+const myMap = L.map("map").setView([22.9074872, 79.07306671], 5);
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const tileLayer = L.tileLayer(tileUrl, { attribution });
+tileLayer.addTo(myMap);
 
-        if (file) {
-            // Read the Excel file
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var data = e.target.result;
-                var workbook = XLSX.read(data, { type: 'binary' });
-                var jsonData = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { header: 1 });
-
-                // Convert Excel data to Mapael format
-                var mapData = {};
-                for (var i = 1; i < jsonData.length; i++) {
-                    var state = jsonData[i][0];
-                    var value = parseInt(jsonData[i][1]);
-
-                    mapData[state] = { value: value, tooltip: { content: state + ': ' + value } };
-                }
-                console.log(mapData);   
-                // Render Mapael map
-                $('#content').mapael({
-                    map: {
-                        name: 'usa_states',
-                        defaultArea: {
-                            attrs: {
-                                fill: '#dddddd',
-                                stroke: '#FFFFFF'
-                            },
-                            attrsHover: {
-                                fill: '#0088db'
-                            }
-                        }
-                    },
-                    areas: mapData
-                });
-            };
-
-            reader.readAsBinaryString(file);
-        }
+function generateList(campusList) {
+  const ul = document.querySelector(".list");
+  ul.innerHTML = "";
+  campusList.forEach((campus) => {
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    const a = document.createElement("a");
+    const p = document.createElement("p");
+    a.addEventListener("click", () => {
+      flyToStore(campus);
     });
+    div.classList.add("campus-item");
+    a.innerText = campus.properties.name;
+    a.href = "#";
+    p.innerText = campus.properties.address;
+
+    div.appendChild(a);
+    div.appendChild(p);
+    li.appendChild(div);
+    ul.appendChild(li);
+  });
+}
+
+generateList(allCampusData);
+
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const { value } = searchInput;
+
+  const filteredCampus = allCampusData.filter((campus) => {
+    return (
+      campus.properties.name.match(new RegExp(value, "gi")) ||
+      campus.properties.address.match(new RegExp(value, "gi"))
+    );
+  });
+  if (filteredCampus.length) {
+    generateList(filteredCampus);
+  } else {
+    const ul = document.querySelector(".list");
+    ul.innerHTML = '<p class="no-result">No Results Found</p>';
+  }
 });
+
+function makePopupContent(campus) {
+  return `
+    <div>
+        <h4>${campus.properties.name}</h4>
+        <p>${campus.properties.address}</p>
+        <p>NAAC: ${campus.properties.naac} grading</p>
+        <div class="phone-number">
+            <a href="tel:${campus.properties.phone}">${campus.properties.phone}</a>
+        </div>
+    </div>
+  `;
+}
+function onEachFeature(feature, layer) {
+  layer.bindPopup(makePopupContent(feature), {
+    closeButton: false,
+    offset: L.point(0, -8),
+  });
+}
+
+var myIcon = L.icon({
+  iconUrl: "school-marker.png",
+  iconSize: [40, 40],
+});
+
+const campussLayer = L.geoJSON(allCampusData, {
+  onEachFeature: onEachFeature,
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon: myIcon });
+  },
+});
+campussLayer.addTo(myMap);
+
+function flyToStore(store) {
+  const lat = store.geometry.coordinates[1];
+  const lng = store.geometry.coordinates[0];
+  myMap.flyTo([lat, lng], 14, {
+    duration: 3,
+  });
+  setTimeout(() => {
+    L.popup({ closeButton: false, offset: L.point(0, -8) })
+      .setLatLng([lat, lng])
+      .setContent(makePopupContent(store))
+      .openOn(myMap);
+  }, 3000);
+}
